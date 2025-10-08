eb48c23d6d9d62d979f9e44149b3c9139e8248b2
Orange Pie <82283728+GaMaDeCa@users.noreply.github.com>
2025-10-07 22:13:54 -0300

    Add files via upload

---
+++ Pascal/BogoSort.pas
@@ -0,0 +1,6 @@
+{ TODO > Nothing
+	I've just finished it and guess what? the program crashed and everything(>70 or 100 lines) got erased, I'm not doing it again, if you want to know what this program does(or was supposed to do):
+		-> Generate a 1 to 10 integer array
+		-> Shuffle the array(the correct shuffle algorithm, swap index i, i2 with tmp to hold a value)
+		-> Sort it randomly(random swap) and checks if it's sorted in every step.
+}
